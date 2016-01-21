072f64b4cb2609d6ac3b7d7ebdc91143a96af684
David <d_rod3663@hotmail.com>
2016-01-21 13:55:34 -0600

    updated resume

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -10,36 +10,65 @@
 
 @section('content')
     <div class ="section">
-    <!-- <blockquote id="quote">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-    <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
-    </blockquote>
+    
+    
+    <h3 style="text-align: center">David Rodriguez</h3>
+    <h4 style="color: grey; text-align: center">Full Stack Web Developer</h4>
+    <h5 style="text-align: center"> San Antonio, TX 78259 | (210) 379-6843 | drod63@gmail.com</h5>
 
+    <br>
+    <h4 style="text-align: center">Why Me?</h4>
+    <h4>
+    <p>• Leadership skills. Over 5 years managing a variety of adults in a fast paced environment.</p>
+    <p>• Dedicated. Eager and ready to help my team in any capacity to get the job done.</p>
+    <p>• Motivated. Committed to bring my knowledge of programming and service to an
+    exceptional company</p>
+    </h4>
+    <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+    <br>
 
-    <dl>
-    <h1><dt>Education</h1></dt>
-    <dd>Madison High School</dd>
-    <dd>Texas A&M University</dd>
-    <dd>Codeup</dd>
-    </dl>
+    <h4><b>Skills</b></h4>
+    <h4>HTML5, CSS3, Javascript, Jquery, AngularJS, Git & Github, MySql, PHP, Laravel Framework</h4>
+    <br>
+    <h4><b>Full Stack Projects</b></h4>
+    <h4><em>borqgaming.com </em></h4>
+    <h4>Text adventure game modeled after the 80s classic, Zork.</h4>
+    <h4>https://github.com/Borq-Gaming/borq.dev</h4>
+    <h4><em>tremendousupside.com </em></h4>
+    <h4>A personal website built to include a little about me as well as a portfolio of small projects and a blog.</h4>
+    <h4><em>Laravel Blog Project </em></h4>
+    <h4>Personal blog built using Laravel. </h4>
+    <br>
 
-    <dl>
-    <h1><dt>Work Experience </h1></dt>
-    <dd>Chick-Fil-A</dd>
-    <dd>...</dd>
-    </dl>
+    <h4><b>Education</b></h4>
+    <h4>Codeup - <em>San Antonio, TX</em></h4>
+    <h4>June 2015-October 2015</h4>
+    <h4>A 4-month long full-stack web development boot camp focused on the LEMP-JS Stack. Practiced agile development, pair-programming 
+    and scrum methods.</h4>
+    <h4>Texas A&M University - <em>College Station, TX</em></h4>
+    <h4>August 2002 – May 2006</h4>
+    <h4>Studied business and economics</h4>
+    <br>
 
-    <dl>
-    <h1><dt>Skills</h1></dt>
-    <dd>...</dd>
-    <dd>...</dd>
-    </dl>
+    <h4><b>Experience</b></h4>
+    <h4>Assistant Manager<h4>
+    <h4><em>Chick-fil-A - San Antonio, TX</em></h4>
+    <h4>January 2006 - June 2015</h4>
+    <ul>
+        <li>Managed daily operations to include employee attendance, retention, equipment maintenance, customer satisfaction, 
+            product inventory, daily safety and food temperature audits and perform restaurant quality assessments. </li>
+        <li>Co-managed employee schedules, new hires, employee performance and benchmark cost saving production. </li>
+        <li>Managed food arrangement for outside sales events, catered events and promotional activities.</li>
+    </ul>
+    <h4>Sales Associate</h4>
+    <h4><em>Albertson's - San Antonio, TX</em></h4>
+    <h4>June 2000 - September 2000</h4>
+    <ul>
+        <li>Worked part time as a bagger and support team member to cashiers and managers. </li>
+        <li>Learned time management between school studies, work and extracurricular activities</li>
+        <li>Learned to effectively communicate with customers and team members.</li>
+    </ul>
 
-    <dl>
-    <h1><dt>Interests</h1></dt>
-    <dd>...</dd>
-    <dd>...</dd>
-    </dl> -->
-    <h1>Currently being updated. For more information please contact me.
     </div>
+
 @stop
